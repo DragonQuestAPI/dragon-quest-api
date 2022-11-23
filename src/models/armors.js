56778446afdb6f18ae93e3c086781728b1831dd2
NaorimSenchai <romainchenais35@gmail.com>
2022-11-23 14:53:52 +0100

    added models

---
+++ src/models/armors.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathToJSON = path.join(__dirname, "../data/armors.json");
+const file = JSON.parse(fs.readFileSync(pathToJSON).toString());
+
+const findAll = async () => {
+  try {
+    const result = await file;
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const findById = async (id) => {
+  try {
+    const result = await file.filter((data) => data.id == id)[0];
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
