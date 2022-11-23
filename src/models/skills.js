1b03c4199f7598e8cc34d6d913a4c21e0f8adbd5
NaorimSenchai <romainchenais35@gmail.com>
2022-11-23 16:09:46 +0100

    added skills models

---
+++ src/models/skills.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathToJSON = path.join(__dirname, "../data/skills.json");
+const file = JSON.parse(fs.readFileSync(pathToJSON).toString());
+
+const findAll = async () => {
+  try {
+    const result = file;
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const findById = async (id) => {
+  try {
+    const result = file.filter((data) => data.id == id)[0];
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
