d6f20d88df996fcde20ff285ac40bb88e5ec15e0
NaorimSenchai <romainchenais35@gmail.com>
2022-11-24 19:55:09 +0100

    updated routes to fit dq games and not only dqiv

--- src/server.js
+++ src/server.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const app = express();
-const fs = require("fs"); // File system
-const path = require("path");
 require("dotenv").config();
-
-const pathMonstersJSON = path.join(__dirname, "/data/monsters.json");
-const monsters = JSON.parse(fs.readFileSync(pathMonstersJSON).toString());
-
-const characterRoutes = require("./routes/characters.js");
-const weaponRoutes = require("./routes/weapons.js");
-const helmetRoutes = require("./routes/helmets.js");
-const armorRoutes = require("./routes/armors.js");
-const shieldRoutes = require("./routes/shields.js");
-const accessoryRoutes = require("./routes/accessories.js");
-const itemRoutes = require("./routes/items.js");
-const monsterRoutes = require("./routes/monsters.js");
 
 app.use(express.json());
 
-app.use("/characters", characterRoutes);
-app.use("/weapons", weaponRoutes);
-app.use("/helmets", helmetRoutes);
-app.use("/armors", armorRoutes);
-app.use("/shields", shieldRoutes);
-app.use("/accessories", accessoryRoutes);
-app.use("/items", itemRoutes);
-app.use("/monsters", monsterRoutes);
+// const characterRoutes = require("./routes/dqiv/characters.js");
+// const weaponRoutes = require("./routes/dqiv/weapons.js");
+// const helmetRoutes = require("./routes/dqiv/helmets.js");
+// const armorRoutes = require("./routes/dqiv/armors.js");
+// const shieldRoutes = require("./routes/dqiv/shields.js");
+// const accessoryRoutes = require("./routes/dqiv/accessories.js");
+// const itemRoutes = require("./routes/dqiv/items.js");
+// const monsterRoutes = require("./routes/dqiv/monsters.js");
+
+// app.use("/characters", characterRoutes);
+// app.use("/weapons", weaponRoutes);
+// app.use("/helmets", helmetRoutes);
+// app.use("/armors", armorRoutes);
+// app.use("/shields", shieldRoutes);
+// app.use("/accessories", accessoryRoutes);
+// app.use("/items", itemRoutes);
+// app.use("/monsters", monsterRoutes);
+
+const dqivRoute = require("./routes/dqiv.js");
+app.use("/dqiv", dqivRoute);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started on http://localhost:${process.env.PORT}`)
