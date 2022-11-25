eb7c6121b926d63afe05a0de1c90419f22785a34
NaorimSenchai <romainchenais35@gmail.com>
2022-11-25 14:58:55 +0100

    fix: added jsdoc

--- src/utils/formatQueryset.js
+++ src/utils/formatQueryset.js
@@ -1,3 +1,9 @@
+/**
+ * Formats a queryset by deleting unallowed properties
+ * @param {object} queryset The queryset containing to properties to format
+ * @param {array<string/>} allowedProperties An array containing the names of the allowed properties
+ * @returns {object} The formatted queryset
+ */
 const formatQueryset = async (queryset, allowedProperties) => {
   const res = {};
   await Object.keys(queryset).forEach((property) => {
