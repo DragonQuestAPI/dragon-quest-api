e4a3dcd14b3ee2b83ceb7b2d9bd352f4a876d947
NaorimSenchai <romainchenais35@gmail.com>
2023-02-27 13:28:36 +0100

    feat(app): added Docker support

--- src/routes/DQ10/game.js
+++ src/routes/DQ10/game.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const model = require(`../../models/DQ8/game.js`);
+const model = require(`../../models/DQ10/game.js`);
 
 router.get("/", async (req, res) => {
   try {
